8e443b31197984ac8fcd974d5a11290849f5831f
Steven Mumford <beaverstever@Stevens-MacBook-Pro.local>
2016-10-27 10:34:50 -0700

    Started Part 1 INCOMPLETE

--- HW05 Actual/HW05 Actual/main.cpp
+++ HW05 Actual/HW05 Actual/main.cpp
@@ -7,10 +7,31 @@
 //
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+T linearSearch(const T list[], T key, int arraysize);
 
 int main() {
     
+    int intList[] = { 1, 2, 5, 4, 6, 9};
+    double doubleList[] = {1.1, 5.4, 6.0, 7.8, 9.0, 3.3};
+    string stringList[] = {"Hi", "Hello", "Hey", "Greetings", "Bonjour", "Hola"};
     
+    cout << "The index of the element is" << linearSearch(intList, 5, 6) << endl;
+    cout << "The index of the element is" << linearSearch(doubleList, 1.1, 6) << endl;
+    cout << linearSearch(stringList, string("Hello"), 6) << endl;
     
     return 0;
+}
+
+template <typename T>
+T linearSearch(const T list[], T key, int arraysize)
+{
+    for(int i=0; i < arraysize; i++)
+        if(key == list[i])
+            return i;
+    return -1;
 }
