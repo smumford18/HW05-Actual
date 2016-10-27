18b6b01a7fabf0f9d8b1ca6e4d48c9896d744290
Steven Mumford <beaverstever@Stevens-MacBook-Pro.local>
2016-10-27 13:59:34 -0700

    Finished part 1 and part 2 Function as desired

--- HW05 Actual/HW05 Actual/main.cpp
+++ HW05 Actual/HW05 Actual/main.cpp
@@ -12,26 +12,58 @@
 using namespace std;
 
 template <typename T>
-T linearSearch(const T list[], T key, int arraysize);
-
-int main() {
-    
-    int intList[] = { 1, 2, 5, 4, 6, 9};
-    double doubleList[] = {1.1, 5.4, 6.0, 7.8, 9.0, 3.3};
-    string stringList[] = {"Hi", "Hello", "Hey", "Greetings", "Bonjour", "Hola"};
-    
-    cout << "The index of the element is" << linearSearch(intList, 5, 6) << endl;
-    cout << "The index of the element is" << linearSearch(doubleList, 1.1, 6) << endl;
-    cout << linearSearch(stringList, string("Hello"), 6) << endl;
-    
-    return 0;
-}
-
-template <typename T>
-T linearSearch(const T list[], T key, int arraysize)
+int linearSearch(const T list[], T key, int arraysize)
 {
     for(int i=0; i < arraysize; i++)
         if(key == list[i])
             return i;
     return -1;
 }
+
+template <typename T>
+bool isSorted(const T list[], int size)
+{
+    bool sorted = true;
+    for(int i=0; i < size; i++)
+    {
+        if(list[i] > list[i+1])
+        {
+            sorted = false;
+            break;
+        }
+    }
+    
+    return sorted;
+}
+
+int main() {
+    
+    int intList[] = { 1, 2, 4, 5, 6, 9};
+    double doubleList[] = {1.1, 5.4, 6.0, 7.8, 9.0, 3.3};
+    string stringList[] = {"Hi", "Hello", "Hey", "Greetings", "Bonjour", "Hola"};
+    
+    cout << "The index of the element is " << linearSearch(intList, 5, 6) << endl;
+    cout << "The index of the element is " << linearSearch(doubleList, 1.1, 6) << endl;
+    cout << "The index of the element is " << linearSearch(stringList, string("Hello"), 6) << endl;
+    cout << endl;
+    
+    if(isSorted(intList, 6) == true)
+        cout << "The list of integers is sorted" << endl;
+    else
+        cout << "The list of integers is not sorted" << endl;
+    
+    if(isSorted(doubleList, 6)== true)
+        cout << "The list of doubles is sorted" << endl;
+    else
+        cout << "The list of doubles is not sorted" << endl;
+    
+    if(isSorted(stringList, 6) == true)
+        cout << "The list of strings is sorted" << endl;
+    else
+        cout << "The list of strings is not sorted" << endl;
+    
+    
+    return 0;
+    
+}
+
