fc6359069093203f3ee17e7b8bc021443b446c8f
Steven Mumford <beaverstever@Stevens-MacBook-Pro.local>
2016-10-27 10:16:52 -0700

    Initial Commit New Xcode Project created

---
+++ HW05 Actual/HW05 Actual/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HW05 Actual
+//
+//  Created by Steven Mumford on 10/27/16.
+//  Copyright © 2016 Steven Mumford. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    
+    
+    
+    return 0;
+}
